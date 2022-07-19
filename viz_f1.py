ef6b8bd8af0e86f7f572342143be5a008ec99ce1
javilavelez <109565987+javilavelez@users.noreply.github.com>
2022-07-18 22:28:32 -0400

    Update viz_f1.py

--- viz_f1.py
+++ viz_f1.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Races are won at the track. Championships are won at the factory." - Mercedes (2019))
-st.write(La cantidad de datos que se capturan, analizan y utilizan para diseñar, construir y conducir los coches de Fórmula 1 es impresionante. Es un deporte global que siguen millones de personas en todo el mundo y es fascinante ver cómo los pilotos se ponen al límite en estos vehículos para convertirse en los corredores más rápidos del mundo.)
+st.title("Races are won at the track. Championships are won at the factory - Mercedes (2019)")
+st.write("La cantidad de datos que se capturan, analizan y utilizan para diseñar, construir y conducir los coches de Fórmula 1 es impresionante. Es un deporte global que siguen millones de personas en todo el mundo y es fascinante ver cómo los pilotos se ponen al límite en estos vehículos para convertirse en los corredores más rápidos del mundo.")
