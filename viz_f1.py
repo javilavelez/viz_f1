49e00f708b71b52718a5af2e2b06fe2620bc1604
javilavelez <109565987+javilavelez@users.noreply.github.com>
2022-07-18 22:36:12 -0400

    Update viz_f1.py

--- viz_f1.py
+++ viz_f1.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 
 st.title("Visualización circuitos y campeones de la F1 1951 - 2022")
-st.markdown("**Races are won at the track. Championships are won at the factory - Mercedes (2019)**")
+st.markdown("***Races are won at the track. Championships are won at the factory - Mercedes (2019)***")
 st.markdown("La F1 es un deporte global que siguen millones de personas en todo el mundo y es fascinante ver cómo los pilotos se ponen al límite en estos vehículos para convertirse en los corredores más rápidos del mundo.")
 st.write("El dataset contiene la información sobre las carreras de Fórmula 1, los pilotos, la clasificación, los circuitos y los campeonatos desde 1951 hasta la última temporada de 2021.")
