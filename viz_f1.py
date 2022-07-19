0a9a6f71b34dd922bb0c532f177dafc3c4308005
javilavelez <109565987+javilavelez@users.noreply.github.com>
2022-07-18 22:30:53 -0400

    Update viz_f1.py

--- viz_f1.py
+++ viz_f1.py
@@ -4,3 +4,5 @@ import numpy as np
 
 st.title("Races are won at the track. Championships are won at the factory - Mercedes (2019)")
 st.write("La cantidad de datos que se capturan, analizan y utilizan para diseñar, construir y conducir los coches de Fórmula 1 es impresionante. Es un deporte global que siguen millones de personas en todo el mundo y es fascinante ver cómo los pilotos se ponen al límite en estos vehículos para convertirse en los corredores más rápidos del mundo.")
+st.markdown('<br></br>')
+st.write("El dataset contiene la información sobre las carreras de Fórmula 1, los pilotos, la clasificación, los circuitos y los campeonatos desde 1950 hasta la última temporada de 2021.")
