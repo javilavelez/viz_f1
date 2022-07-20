ecaaa9982dfe1df5adc40184e0bca995cda41bd9
javilavelez <109565987+javilavelez@users.noreply.github.com>
2022-07-20 13:27:14 -0400

    Update viz_f1.py

--- viz_f1.py
+++ viz_f1.py
@@ -9,34 +9,29 @@ st.markdown("La F1 es un deporte global que siguen millones de personas en todo 
 st.markdown('<br></br>',unsafe_allow_html=True)
 st.markdown("**Dataset**")
 st.write("El dataset contiene la información sobre las carreras de Fórmula 1, los pilotos, la clasificación, los circuitos y los campeonatos desde 1951 hasta la última temporada de 2021.")
+st.markdown('<br></br>',unsafe_allow_html=True)
+st.markdown("**Descripción del dataset**")
 
-dataset = [['driverRef', 'String', 'Identificador del piloto'], 
-           ['driver_forename', 'String', 'Nombre del piloto'], 
-           ['driver_surname', 'String', 'Apellido del piloto'], 
-           ['driver_dob', 'Date', 'Fecha de nacimiento del piloto'], 
-           ['driver_nationality', 'String', 'Nacionalidad del piloto'], 
-           
-           ['raceId', 'Number', 'Identificador de la carrera'], 
-           ['result_number', 'Number', 'Nacionalidad del piloto'], 
-           
-           ['start_position', 'String', 'Nacionalidad del piloto'], 
-           ['final_position', 'String', 'Nacionalidad del piloto'], 
-           ['rank', 'String', 'Nacionalidad del piloto'], 
-           
-           ['status', 'String', 'Nacionalidad del piloto'], 
-           ['circuit_reference', 'String', 'Nacionalidad del piloto'], 
-           ['circuit_name', 'String', 'Nacionalidad del piloto'],
-           
-           ['circuit_location', 'String', 'Nacionalidad del piloto'], 
-           ['circuit_country', 'String', 'Nacionalidad del piloto'], 
-           ['circuit_lat', 'String', 'Nacionalidad del piloto'], 
-           
-           ['circuit_lng', 'String', 'Nacionalidad del piloto'], 
-           ['year', 'String', 'Nacionalidad del piloto'], 
-           ['race_name', 'String', 'Nacionalidad del piloto'],
-           
-           ['race_date', 'String', 'Nacionalidad del piloto'], 
-           ['race_time', 'String', 'Nacionalidad del piloto'],
-          ]
+dataset = [
+	['raceId','Number','ID de la carrera'],
+	['race_year','Number','Año de la carrera'],
+	['round','Number','Round del año'],
+	['race_name','String','Nombre de la carrera'],
+	['race_date','Date','Fecha de la carrera'],
+	['driverId','Number','ID del piloto'],
+	['driv_name','String','Nombre del piloto'],
+	['driv_surname','String','Apellido del piloto'],
+	['driv_dob','Date','Fecha de nacimiento del piloto'],
+	['driv_nationality','String','Nacionalidad del piloto'],
+	['constructorId','Number','ID del fabricante'],
+	['cons_name','String','Nombre del fabricante'],
+	['cons_nationality','String','Nacionalidad del fabricante'],
+	['start_position','Number','Posición inicial del piloto en la carrera'],
+	['final_position','Number','Posición final del piloto en la carrera'],
+	['rank','Number','Ranking del piloto']
+        ]
+
+df_data = pd.DataFrame(dataset, columns=['Variable','Tipo','Descripción'])
+st.table(df_data)
 
 														
