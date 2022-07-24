42a0711fdcd4c8b97f16bfb43d4d47fd91cea0cb
javilavelez <109565987+javilavelez@users.noreply.github.com>
2022-07-24 17:22:59 -0400

    Update viz_f1.py

--- viz_f1.py
+++ viz_f1.py
@@ -6,9 +6,25 @@ import plotly.express as px
 
 st.title("Circuitos, Pilotos y Fabricantes de la (F1 1951 - 2022)")
 st.markdown("***Races are won at the track. Championships are won at the factory - Mercedes (2019)***")
-st.markdown('<br> La F1 es un deporte global que siguen millones de personas en todo el mundo y es fascinante ver cómo los pilotos se ponen al límite en estos vehículos para convertirse en los corredores más rápidos del mundo. </br>',unsafe_allow_html=True)
+st.markdown('La F1 es un deporte global que siguen millones de personas en todo el mundo y es fascinante ver cómo los pilotos se ponen al límite en estos vehículos para convertirse en los corredores más rápidos del mundo.')
 
-st.markdown("**Dataset**")
+st.markdown("**Contexto**")
+st.markdown('El Campeonato Mundial de Fórmula 1 es la principal competición de automovilismo internacional y el campeonato de deportes de motor más popular y prestigioso del mundo.')
+st.markdown('Cada carrera de la F1 se le denomina Gran Premio y el torneo que las agrupa es el **Campeonato Mundial de Fórmula 1**. La mayoría de los circuitos de carreras donde se celebran los Grandes Premios son autódromos, aunque también se utilizan circuitos callejeros. A su vez, los automóviles utilizados son monoplazas con la última tecnología disponible, siempre limitadas por un reglamento técnico; algunas mejoras que fueron desarrolladas en la Fórmula 1 terminaron siendo utilizadas en automóviles comerciales, como el freno de disco. El inicio de la Fórmula 1 moderna se remonta al año 1950, en el que participaron escuderías como Ferrari, Alfa Romeo y Maserati, algunas reemplazadas por otras nuevas como McLaren, Williams y Red Bull. Por su parte, los pilotos deben contar con la superlicencia de la FIA para competir, que se obtiene por los resultados en otros campeonatos.')
+st.markdown('El siguiente proyecto tiene como propósito ilustrar la trayectoria de los pilotos y fabricantes de autos de la F1 a través del tiempo.')
+
+st.markdown("**Características del dominio**")
+st.markdown('El gráfico se realizará para:')
+st.markdown('1. Todas las personas (hombres, mujeres y no binarios)')
+st.markdown('2. Personas conocedoras de la F1, pero no necesariamente expertas en este tópico.')
+st.markdown('3. Personas con conocimeinto sobre los números (tipo) y sus aplicaciones (semántica), es decir, saben que dependiendo del contexto el número puede ser año así como también puede ser posición. (Gráfica no diseñada para niños que no saben leer o escribir, por ejemplo).')
+
+st.markdown("**Abstracción de tareas**")
+st.markdown('1. Presentar la evolución de pilotos y de los fabricantes de los autos en el ranking de la F1 a través del tiempo.')
+st.markdown('2. Descubrir si los mejores fabricantes de autos han cambiado en el tiempo.')
+st.markdown('3. Identificar a los mejores pilotos desde 1950 - 2021.')
+
+st.markdown("**Abstracción de datos**")
 st.markdown('El dataset contiene la información sobre las carreras de Fórmula 1, los pilotos, la clasificación, los circuitos y los campeonatos desde 1951 hasta la última temporada de 2021.')
 
 st.markdown("**Descripción del dataset**")
@@ -34,17 +50,6 @@ dataset = [
 
 df_data = pd.DataFrame(dataset, columns=['Variable','Tipo','Descripción'])
 st.table(df_data)
-
-st.markdown("**Características del dominio**")
-st.markdown('El gráfico se realizará para:')
-st.markdown('1. Todas las personas (hombres, mujeres y no binarios)')
-st.markdown('2. Personas conocedoras de la F1, pero no necesariamente expertas en este tópico.')
-st.markdown('3. Personas con conocimeinto sobre los números (tipo) y sus aplicaciones (semántica), es decir, saben que dependiendo del contexto el número puede ser año así como también puede ser posición. (Gráfica no diseñada para niños que no saben leer o escribir, por ejemplo).')
-
-st.markdown("**Abstracción de tareas**")
-st.markdown('1. Presentar la evolución de pilotos y de los fabricantes de los autos en el ranking de la F1 a través del tiempo.')
-st.markdown('2. Descubrir si los mejores fabricantes de autos han cambiado en el tiempo.')
-st.markdown('3. Identificar a los mejores pilotos desde 1950 - 2022.')
 
 # Visualization
 st.markdown('<br></br>',unsafe_allow_html=True)
